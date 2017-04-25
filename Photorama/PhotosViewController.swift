8d76a2720176c5f8c8408b6802640e6fc08442e8
Sean Melnick Kelly <seanmelnickkelly@seans-macbook-pro.local>
2017-04-24 23:07:08 -0400

    Completed through pg. 358

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Sean Melnick Kelly on 4/24/17.
+//  Copyright © 2017 Sean Melnick Kelly. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+}
