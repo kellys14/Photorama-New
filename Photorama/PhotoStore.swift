40887bb1eb28986d1b607d27ab18e30755220d56
Sean Melnick Kelly <seanmelnickkelly@seans-macbook-pro.local>
2017-04-25 01:12:48 -0400

    Chapter 20 Base Completed

--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -6,7 +6,16 @@
 //  Copyright © 2017 Sean Melnick Kelly. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum ImageResult { // pg. 371
+    case success(UIImage)
+    case failure(Error)
+}
+
+enum PhotoError: Error { // pg. 371
+    case imageCreationError
+}
 
 enum PhotosResult { // pg. 364
     case success([Photo])
@@ -20,25 +29,60 @@ class PhotoStore {
         return URLSession(configuration: config)
     }()
     
-    func fetchInterestingPhotos() { // pg. 358, 362
+    func fetchInterestingPhotos(completion: @escaping (PhotosResult) -> Void) { // pg. 358, 362, 368
         let url = FlickrAPI.interestingPhotosURL
         let request = URLRequest(url: url)
         let task = session.dataTask(with: request) {
             (data, response, error) -> Void in
             
-            if let jsonData = data {
-                do { // pg. 362
-                    let jsonObject = try JSONSerialization.jsonObject(with: jsonData, options: [])
-                    print(jsonObject)
-                } catch let error {
-                    print("Error creating JSON object: \(error)")
-                }
-            } else if let requestError = error {
-                print ("Error fetching interesting photos: \(requestError)")
-            } else {
-                print("Unexpected error with the request")
+            let result = self.processPhotosRequest(data: data, error: error) // pg. 368
+            OperationQueue.main.addOperation {
+                completion(result) // pg. 368
             }
         }
         task.resume()
+    }
+    
+    private func processPhotosRequest(data: Data?, error: Error?) -> PhotosResult { // pg. 368
+        // Method that will process JSON data that is returned from the web request
+        guard let jsonData = data else {
+            return .failure(error!)
+        }
+        
+        return FlickrAPI.photos(fromJSON: jsonData)
+    }
+    
+    func fetchImage(for photo: Photo, completion: @escaping (ImageResult) -> Void) { // pg. 371, 372
+        // Method to download image data
+        
+        let photoURL = photo.remoteURL
+        let request = URLRequest(url: photoURL)
+        
+        let task = session.dataTask(with: request) {
+            (data, response, error) -> Void in
+            
+            let result = self.processImageRequest(data: data, error: error) // pg. 372
+            OperationQueue.main.addOperation { // pg. 374
+                completion(result) // pg. 372
+            }
+        }
+        task.resume()
+        
+    }
+    
+    private func processImageRequest(data: Data?, error: Error?) -> ImageResult { // pg. 372
+        // Method that processes data from the web service request into an image
+        guard
+            let imageData = data,
+            let image = UIImage(data: imageData) else {
+                
+                // Couldn't create an image
+                if data == nil {
+                    return .failure(error!)
+                } else {
+                    return .failure(PhotoError.imageCreationError)
+                }
+        }
+        return .success(image)
     }
 }
--- Photorama/PhotosViewController.swift
+++ Photorama/PhotosViewController.swift
@@ -17,6 +17,33 @@ class PhotosViewController: UIViewController {
         super.viewDidLoad()
         
         // Starts web exchange for the photos when the view controller comes onscreen - pg. 359
-        store.fetchInterestingPhotos()
+        store.fetchInterestingPhotos {
+            (PhotosResult) -> Void in // pg. 370
+            
+            switch PhotosResult { // pg. 370
+            case let .success(photos):
+                print("Successfully found \(photos.count) photos.")
+                if let firstPhoto = photos.first { // pg. 372
+                    self.updateImageView(for: firstPhoto)
+                }
+            case let .failure(error):
+                print("Error fetching interesting photos: \(error)")
+            }
+            
+        }
+    }
+    
+    func updateImageView(for photo: Photo) {
+        // Method that will fetch image and display to image view
+        store.fetchImage(for: photo) {
+            (imageResult) -> Void in
+            
+            switch imageResult { // pg. 372
+            case let .success(image):
+                self.imageView.image = image
+            case let .failure(error):
+                print("Error downloading image: \(error)")
+            }
+        }
     }
 }
